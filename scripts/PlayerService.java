8e66e7acea1900ef1d42eae9a7121d9bcab260ec
BrutusGamesTeam <oficiallazarentos@gmail.com>
2024-10-10 01:50:52 -0300

    Create the PlayerService.java File

---
+++ scripts/PlayerService.java
@@ -0,0 +1,18 @@
+// Package Declaration
+package JAVARuntime;
+
+// Explicit Imports (ITsMagic Engine)
+import JAVARuntime.Component;
+import JAVARuntime.SpatialObject;
+import JAVARuntime.AsyncTask;
+import JAVARuntime.AsyncRunnable;
+
+// the Players Service is a Useful Core-Service with very Functions (Such as Players Handle)
+public class PlayerService
+{
+	// Private Constructor (Empty)
+	private PlayerService()
+	{
+		// Make Nothing
+	}
+}
