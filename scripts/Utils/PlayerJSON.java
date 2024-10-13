cd24a6c7709d29e28d58f73f9fd472ad2a6d1638
BrutusGamesTeam <oficiallazarentos@gmail.com>
2024-10-13 06:24:46 -0300

    Create PlayerJSON.java

---
+++ scripts/Utils/PlayerJSON.java
@@ -0,0 +1,15 @@
+// Declaração do Pacote de Código
+package JAVARuntime;
+
+// Importações Úteis (Explícitas do Jogo Padrão)
+import JAVARuntime.Player;
+
+// Importações Úteis (Explícitas do Java)
+import java.lang.Object;
+import java.lang.String;
+
+// Classe Responsável por Integrar o Sistema de JSON do Jogador para o Jogo
+public class Player
+{
+    
+}
